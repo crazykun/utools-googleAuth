28c3b5fd85832a2f2fb2ebf5d7acabce22f78131
jilikun <jilikun@gmail.com>
2024-06-17 19:25:20 +0800

    导入导出功能

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,15 @@
-// const jsSHA = require("jssha");
-// window.jsSHA = function () {
-//     return new jsSHA('SHA-1', 'BYTES');
-//   }	
-  
+const { readFileSync, writeFileSync } = require('fs')
+
+window.readFile = function (file) {
+    return  readFileSync(file, 'utf8')
+}
+   
+
+window.writeFile = function (filePath, data) {
+    try {
+        writeFileSync(filePath, data)
+        return ''
+    } catch (err) {
+        return err
+    }
+}	
